9af25e6ad3d7d2505477764ec5c24e7154c1efb5
jigar-panchal-ncgm <jigar.panchal@supratechlabs.com>
2021-09-18 23:18:26 +0530

    Updates

---
+++ precache-manifest.852ddbafd21943fbddf9028d8bb4c07a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa68d56a83a6220b8536d3e5e356ee7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76143ad7af91b380fb48",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "bf788a21ff7814ba6feb",
+    "url": "/static/js/2.78ef4ad9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.78ef4ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76143ad7af91b380fb48",
+    "url": "/static/js/main.aa2afdcb.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "e069faedba668d18470a06f7ed50bf46",
+    "url": "/static/media/kp.e069faed.jpg"
+  },
+  {
+    "revision": "b1946ac92492d2347c6235b4d2611184",
+    "url": "/static/media/resume.b1946ac9.pdf"
+  },
+  {
+    "revision": "b1d66dcbc892b3bfd541e38cb7f7c1c5",
+    "url": "/static/media/stallion.b1d66dcb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.50440b125672f88625daaa5a639e1fbe.js"
+  "/precache-manifest.852ddbafd21943fbddf9028d8bb4c07a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
